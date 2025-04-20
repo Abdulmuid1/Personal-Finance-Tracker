0c1e968081da434a60146d45c0176dc76407477f
Abdulmuid1 <olaniyanabdulmuid@gmail.com>
2025-04-20 01:38:16 -0600

    Venv Changes
    Installed packages in the virtual environment

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\olani\OneDrive\finance_tracker\venv
+set VIRTUAL_ENV=C:\Users\olani\OneDrive\Documents\My_Projects\Personal-Finance-Tracker\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
